b3d60e3450f9e5320626d302c5c4e45d718c2091
yuanfuqing <67078942+yuanfuqing@users.noreply.github.com>
2022-07-27 21:12:42 +0200

    Update web.py

--- web.py
+++ web.py
@@ -5,6 +5,3 @@ conn = connect()
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 st.write("Hello")
-sheet_url = st.secrets["public_gsheets_url"]
-rows = conn.execute(query, headers=1)
-st.write(sheet_url)
