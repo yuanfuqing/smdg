137472f5afb48e2c88491f92f5e257a6bee3b5f1
yuanfuqing <67078942+yuanfuqing@users.noreply.github.com>
2022-07-27 20:59:58 +0200

    Update web.py

--- web.py
+++ web.py
@@ -11,3 +11,5 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 st.write("Hello")
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
