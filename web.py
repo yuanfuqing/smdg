23705d632c3e34b9aedddb9f6c3ce7b416b90af4
yuanfuqing <67078942+yuanfuqing@users.noreply.github.com>
2022-07-27 20:47:48 +0200

    Update web.py

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 from gsheetsdb import connect
 # Create a connection object.
 conn = connect()
-
+public_gsheets_url = "https://docs.google.com/spreadsheets/d/1pCrJ9O3T6le6ptl-xUbbdGLhJ_J3SK6zgldJ5MJw6pA/edit?usp=sharing"
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
